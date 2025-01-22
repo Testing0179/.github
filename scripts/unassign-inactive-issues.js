1640b9ac3fafa8599c1d80eedbf42c6039454696
Sukhvir Singh <sukhvirkooner05@gmail.com>
2025-01-23 01:56:00 +0530

    changes

--- scripts/unassign-inactive-issues.js
+++ scripts/unassign-inactive-issues.js
@@ -22,7 +22,7 @@ const formatUnassignments = (unassignments) => {
   // Format the grouped unassignments
   return Object.values(groupedByIssue)
     .map(({ users, repo, issueNumber, issueUrl }) => 
-      `• ${users.map(u => `@${u}`).join(', ')} from <${issueUrl}|${repo}#${issueNumber}>`
+      `• ${users.map(u => `@${u}`).join(', ')} from <${issueUrl}|${repo}#${issueNumber}>\n`
     )
     .join('\n');
 };
